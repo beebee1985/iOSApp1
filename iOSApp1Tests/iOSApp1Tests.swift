7c65b260e8f1c6799f1dde0c12482e05211c036a
beebee1985 <rukkybb85@gmail.com>
2025-09-19 16:47:47 +0000

    Add unit and UI tests for iOSApp1

---
+++ iOSApp1Tests/iOSApp1Tests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import iOSApp1
+
+final class iOSApp1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
